616141045113cce30ff0b9feb9c13cd78dc80d3a
Colby M. White <colby.white@utexas.edu>
2015-01-23 23:44:26 -0600

    better name for global; better usage

--- lib/timbrel/config.rb
+++ lib/timbrel/config.rb
@@ -2,4 +2,4 @@ require 'yaml'
 
 default_config_yaml = '~/.timbrel.yaml'
 config_yaml = ENV['TIMBREL_CONFIG'] || default_config_yaml
-CONFIG = YAML.load_file(File.expand_path(config_yaml))
+TIMBREL_CONFIG = YAML.load_file(File.expand_path(config_yaml))
